dd9eda33e3b0c671c62a024629e09a96255f8ec0
yanmada <92165034+yanmada@users.noreply.github.com>
2021-11-02 15:41:48 -0300

    atualizar

--- exercicios/aula014ex/ex016/script.js
+++ exercicios/aula014ex/ex016/script.js
@@ -3,7 +3,9 @@ function clicar() {
     var fim = window.Number(document.getElementById('txtfim').value)
     var passo = window.Number(document.getElementById('txtpasso').value)
     var res = window.document.getElementsByClassName('res')[0]
-    if (passo == 0) {
+    if (fim == 0 || inicio == length) {
+        res.innerHTML = 'Impossível contar!'
+    } else if (passo == 0) {
         window.alert('Passo inválido! Considerando PASSO 1')
         if (inicio < fim) {
             var c = inicio
@@ -22,8 +24,6 @@ function clicar() {
             }
             res.innerHTML += '&#x1F3C1'
         }
-    } else if (fim == 0 || inicio == length) {
-        res.innerHTML = 'Impossível contar!'
     } else if (inicio < fim) {
         var c = inicio
         res.innerHTML = '<p>Contando:</p>'
--- tests/exercicio-parte3/ex016/script.js
+++ tests/exercicio-parte3/ex016/script.js
@@ -3,7 +3,10 @@ function contar() {
     var fim = window.Number(document.getElementById('txtfim').value)
     var passo = window.Number(document.getElementById('txtpasso').value)
     var res = window.document.getElementsByClassName('res')[0]
-    if (passo == 0) {
+    var nada = ''
+    if (fim == 0 || inicio == nada) {
+        res.innerHTML = '<p>Impossível contar!</p>'
+    } else if (passo == 0) {
         window.alert('Passo inválido! Considerando PASSO 1')
         if (inicio < fim) {
             res.innerHTML = '<p>Contando:</p>'
@@ -22,8 +25,6 @@ function contar() {
             }
             res.innerHTML += '&#x1F3C1'
         }
-    } else if (fim == 0) {
-        res.innerHTML = '<p>Impossível contar!</p>'
     } else if (inicio < fim) {
         res.innerHTML = '<p>Contando:</p>'
         var c = inicio
