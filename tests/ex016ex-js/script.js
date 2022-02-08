c8758fe6caff74be2df8c8d98f256387610ce8f6
Yama <92165034+yanmada@users.noreply.github.com>
2022-02-08 15:57:37 -0300

    atualizando javascript

--- tests/ex016ex-js/script.js
+++ tests/ex016ex-js/script.js
@@ -1,18 +1,18 @@
 let tnum = window.document.getElementById('txtnum')
-let res = window.document.querySelector('div.res')
 let val = []
+let res = window.document.querySelector('div.res')
 
 function adicionar() {
-    let num = Number(tnum.value)
-    let tab = window.document.getElementById('tabela')
-    let item = document.createElement('option')
+        let num = Number(tnum.value)    
+        let tab = window.document.getElementById('tabela')
+        let item = document.createElement('option')
     if (tnum.value.length == 0 || tnum.value < 1 || tnum.value > 100) {
         window.alert('Valor inválido ou já encontrado na lista.')
     } else {
-        item.text = `Valor ${num} adicionado.`
-        item.value = `Posições${val.length}`
-        tab.appendChild(item)
         val.push(num)
+        item.text = `Valor ${num} adicionado.`
+        item.value = `Posição${val.length}`
+        tab.appendChild(item)
         res.innerHTML = ''
     }
 }
@@ -22,21 +22,21 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar!')
     } else {
         res.innerHTML = `<p>Ao todo, temos ${val.length} números cadastrados.</p>`
-        
+
         let maior = Math.max.apply(null, val)
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-        
+
         let menor = Math.min.apply(null, val)
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
         
-        let total = val.reduce(function(total, valor) {
-            return total + valor
+        let soma = val.reduce(function(soma, valor) {
+            return soma + valor
         })
-        res.innerHTML += `<p>Somando todos os valores temos ${total}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>` 
         
         let media = val.reduce(function(media, numero) {
-            return media + numero
+            return (media + numero)
         })
-        res.innerHTML += `A média dos valores digitados é ${media / val.length}.` 
+        res.innerHTML += `<p>A média dos valores digitados é ${media / val.length}.</p>`
     }
 }
