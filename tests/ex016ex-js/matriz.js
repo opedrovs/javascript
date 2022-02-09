8c476e3521d9c04e1dfc3b66ab347f7ea3817384
Yama <92165034+yanmada@users.noreply.github.com>
2022-02-09 16:40:08 -0300

    atualizando tests

---
+++ tests/ex016ex-js/matriz.js
@@ -0,0 +1,7 @@
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+console.log(matriz[2][1])
--- tests/ex016ex-js/script.js
+++ tests/ex016ex-js/script.js
@@ -1,23 +1,19 @@
 let tnum = window.document.getElementById('txtnum')
-const val = []
-
-let res = window.document.querySelector('div.res')
+let val = []
+let res = window.document.getElementsByClassName('res')[0]
 
 function adicionar() {
-        let num = Number(tnum.value)    
-        let tab = window.document.getElementById('tabela')
-        let item = document.createElement('option')
-    if (tnum.value.length == 0 || tnum.value < 1 || tnum.value > 100) {
+    let num = Number(tnum.value)
+    let tab = window.document.getElementById('tabela')
+    let item = document.createElement('option')
+    if (num.length == 0 || num < 1 || num > 100) {
         window.alert('Valor inválido ou já encontrado na lista.')
     } else {
         val.push(num)
-        const unicaVal = val.filter(function(ele, pos) {
-            return val.indexOf(ele) == pos;
-        })
         item.text = `Valor ${num} adicionado.`
-        item.value = `Posição${val.length}`
+        item.value = `Posições ${val.length}`
         tab.appendChild(item)
-        res.innerHTML = `${unicaVal}`
+        res.innerHTML = ''
     }
 }
 
@@ -26,21 +22,21 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar!')
     } else {
         res.innerHTML = `<p>Ao todo, temos ${val.length} números cadastrados.</p>`
-
+        
         let maior = Math.max.apply(null, val)
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-
+        
         let menor = Math.min.apply(null, val)
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-        
-        let soma = val.reduce(function(soma, valor) {
-            return soma + valor
+
+        let soma = val.reduce(function(soma, numero) {
+            return soma + numero
         })
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>` 
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
         
-        let media = val.reduce(function(media, numero) {
-            return (media + numero)
+        let media = val.reduce(function(media, valor) {
+            return media + valor
         })
-        res.innerHTML += `<p>A média dos valores digitados é ${media / val.length}.</p>`
+        res.innerHTML += `A média dos valores digitados é ${media / val.length}.`
     }
 }
