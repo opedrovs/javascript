2cac3207a478ef9c1dbdf35ee0fc58ba409e5e82
Yama <92165034+pvsanches@users.noreply.github.com>
2022-05-15 22:28:37 -0300

    melhorando a solução do exercício

--- exercicios-completo/ex028/script.js
+++ exercicios-completo/ex028/script.js
@@ -1,3 +1,4 @@
+/*
 let res = window.document.getElementById('res')
 function fatorial() {
     let n = Number(window.document.getElementById('num').value)
@@ -20,6 +21,31 @@ function fatorial() {
                 res.innerHTML += `${c} x `
             }
             res.innerHTML += ` 1 = <strong>${fat.toLocaleString('pt-BR')}</strong>`
+        }
+    }
+}
+*/
+
+let res = window.document.getElementById('res')
+function fatorial() {
+    let num = Number(window.document.getElementById('num').value)
+    
+    if (num < 1 || num > 21) {
+        window.alert('[ERRO] Coloque um número que não seja menor que 1 e maior que 21.')
+    } else {
+        res.innerHTML += `<h2>Calculando ${num}</h2>`
+        
+        if (num == 1) {
+            res.innerHTML += `<p>1 = <strong>1</strong></p>`
+        } else {
+            let fat = 1
+            for (let cont = num; cont > 0; cont--) {
+                res.innerHTML += ` ${cont}`
+                res.innerHTML += cont > 1 ? ' x ' : ' = '
+                fat *= cont
+            }
+            
+            res.innerHTML += `<strong>${fat.toLocaleString('pt-BR')}</strong>`
         }
     }
 }
