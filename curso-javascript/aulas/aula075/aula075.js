20df0e7409c4e7492788fa70dca7abb108d90a37
Yama <92165034+pvsanches@users.noreply.github.com>
2022-05-27 20:18:15 -0300

    adicionando resolução aula075 js

--- curso-javascript/aulas/aula075/aula075.js
+++ curso-javascript/aulas/aula075/aula075.js
@@ -1,5 +1,6 @@
 // Manipulando Prototypes
 
+/*
 // new Object -> Object.prototype
 const objA = {
     chaveA: 'A'
@@ -8,13 +9,70 @@ const objA = {
 
 const objB = {
     chaveB: 'B'
-    // __proto__: Object.prototype
+    // __proto__: objA
 }
+
+const objC = new Object();
+objC.chaveC = 'C';
 
 Object.setPrototypeOf(objB, objA); // Configurar o prototype
 // Configurar prototype de quem?      =  objB
 // Quem vai ser o prototype de objB?  =  objA
+Object.setPrototypeOf(objC, objB);
 
 // Agora, mesmo eu não tendo essa chaveA em objB, eu posso acessar ela pelo objB
+// console.log(objB.chaveA);
+console.log(objC.chaveC);
+*/
 
-console.log(objB.chaveA);
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+
+    // Não vamos criar métodos aqui, pois agora temos o prototype do Produto para isso, não é muito perfomático isso, é melhor colocar métodos fora da sua função construtora
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+// Literal
+const p2 = {
+    nome: 'Caneca',
+    preco: 15 // Aqui o preço precisa bater com as das funções
+}
+Object.setPrototypeOf(p2, Produto.prototype);
+p2.aumento(15);
+// console.log(p1);
+// console.log(p2);
+
+// Nós podemos criar um objeto e já setar o prototype dele de cara
+
+// const p3 = Object.create(Produto.prototype);
+// p3.preco = 113;
+// p3.aumento(10);
+// console.log(p3);
+
+const p3 = Object.create(Produto.prototype, {
+    // Criando objeto, setando prototype (o pai do objeto que estamos criando), as chaves e seus configurações
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 99
+    },
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 43
+    }
+});
+p3.aumento(10);
+console.log(p3);
