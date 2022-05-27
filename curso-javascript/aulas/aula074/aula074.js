260d250d07e59d41b3f48a99c023b5f549f65daf
Yama <92165034+pvsanches@users.noreply.github.com>
2022-05-27 15:59:14 -0300

    Update aula074.js

--- curso-javascript/aulas/aula074/aula074.js
+++ curso-javascript/aulas/aula074/aula074.js
@@ -7,7 +7,7 @@ function Pessoa(nome, sobrenome) {
 
 // instância
 const pessoa1 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
-const pessoa2 = new Pessoa('Maria', 'A.'); // <-
+const pessoa2 = new Pessoa('Maria', 'A.'); // <- Pessoa = Função construtora
 
 console.dir(pessoa1);
 console.dir(pessoa2);
