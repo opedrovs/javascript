82278ca492134ddbb308fe105026b05b48ae5bce
Yama <92165034+pvsanches@users.noreply.github.com>
2022-05-27 15:58:56 -0300

    pequenas modificações

--- curso-javascript/aulas/aula074/aula074.js
+++ curso-javascript/aulas/aula074/aula074.js
@@ -1,4 +1,13 @@
 // Construtora -> molde (classe)
 function Pessoa(nome, sobrenome) {
-    
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.nomeCompleto = () => { this.nome + ' ' + this.sobrenome };
 }
+
+// instância
+const pessoa1 = new Pessoa('Luiz', 'O.'); // <- Pessoa = Função construtora
+const pessoa2 = new Pessoa('Maria', 'A.'); // <-
+
+console.dir(pessoa1);
+console.dir(pessoa2);
