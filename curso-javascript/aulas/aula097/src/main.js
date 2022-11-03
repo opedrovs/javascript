633b8473b3894e34272fdbf50ac009d027269975
Yama <92165034+pvsanches@users.noreply.github.com>
2022-11-03 18:50:47 -0300

    adicionando modificações aula097

--- curso-javascript/aulas/aula097/src/main.js
+++ curso-javascript/aulas/aula097/src/main.js
@@ -1,6 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-// Para quando for 
+// Para quando sua aplicação precisa rodar em navegadores mais antigo, é bom deixar importado acima. (O problema está relacionado com código muito moderno de Javascript sendo convertido para código muito antigo, o que causa erros com Promises e Async Await).
 
 
 import executa from './modules/promises';
