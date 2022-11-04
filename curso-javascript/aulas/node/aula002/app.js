e9bd31d29f4d55a839d20f858af49e3bbd8569aa
Yama <92165034+pvsanches@users.noreply.github.com>
2022-11-04 15:52:25 -0300

    adicionando modificações na aula002 node

--- curso-javascript/aulas/node/aula002/app.js
+++ curso-javascript/aulas/node/aula002/app.js
@@ -6,6 +6,15 @@
 // console.log(n);
 
 // Importando classe
-const Cachorro = require('./z/mod2');
+const Cachorro = require('./z/mod2'); // A extensão .js é opcional
 const c1 = new Cachorro('Pintcher');
 c1.latir();
+
+// console.log(__filename); // Nome do arquivo atual
+// console.log(__dirname); // Nome da pasta atual 
+// Ambos são caminhos absolutos, vão desde a raiz do sistema, até a pasta ou arquivo requisitado
+
+// const path = require('path');
+// Exemplo, eu queira navegar, mas não sei em qual sistema meu programa vai rodar (como Windows), e daí gostaria que ele resolvesse o caminho para chegar no caminho correto (utilizando barras)
+// console.log(__dirname);
+// console.log(path.resolve(__dirname, '..', '..'));
