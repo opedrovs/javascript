eec0ab1e7afa35838b76dadee69ee1fa16225b69
Yama <92165034+pvsanches@users.noreply.github.com>
2022-11-08 18:04:33 -0300

    adicionando aula008 node

--- curso-javascript/aulas/node/aula008/server.js
+++ curso-javascript/aulas/node/aula008/server.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true })); // Essa linha serve para tratar, sem ele, ele não irá tratar do body das requisições, ou seja, qualquer POST ou PUT que enviar via formulário, não irá ser tratado, vai vir como undefined
+
 app.get('/', (req, res) => {
     res.send(`
     <form action="/" method="POST">
     Nome do cliente: <input type="text" name="nome">
+    Outro campo: <input type="text" name="outrocampo">
     <button>Olá mundo</button>
     </form>
     `);
 });
 
-app.post('/', (req, res) => {
-    res.send('Recebi o formulário');
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+    // params -> /profiles/3
+    // query  -> /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.query.facebookprofile);
 });
 
-app.get('/contato', (req, res) => {
-    res.send('Obrigado por entrar em contato com a gente.');
+app.post('/', (req, res) => {
+    console.log(req.body); // vai vir chaves de acordo com que veio no name do formulário
+    res.send(`O que você me enviou foi: ${req.body.nome}`);
 });
 
 app.listen(3500, () => {
