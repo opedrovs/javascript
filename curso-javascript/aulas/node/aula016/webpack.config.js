e4ec2a982316d4e8ec02f19b6653daddb5239d2d
Yama <92165034+pvsanches@users.noreply.github.com>
2022-11-11 20:26:59 -0300

    adicionando modificações node

--- curso-javascript/aulas/node/aula016/webpack.config.js
+++ curso-javascript/aulas/node/aula016/webpack.config.js
@@ -4,14 +4,14 @@ const path = require('path');   // CommonJS
 // Importar módulo
 module.exports = {
     mode: 'production',
-    entry: './frontend/main.js', // pasta onde queremos pegar os arquivos
+    entry: './frontend/main.js',
     output: {
         path: path.resolve(__dirname, 'public', 'assets', 'js'),
         filename: 'bundle.js'
-    }, // a pasta onde jogue meus arquivos
+    },
     module: {
         rules: [{
-            exclude: /node_modules/, // Exclue a pasta node_modules (eu não quero que o webpack analise a pasta do node, pois podera deixar o sistema muito lento)
+            exclude: /node_modules/,
             test: /\.js$/,
             use: {
                 loader: 'babel-loader',
@@ -22,13 +22,7 @@ module.exports = {
         }, {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
-        }] // regras
+        }]
     },
-    devtool: 'source-map' // Ele faz o mapeamento, caso ocorra algum erro, ele vai mapear o arquivo original
-
-    /*
-    No output, o path vai resolver o caminho absoluto, e o resolve() ele vai resolver o caminho que daremos a ele, e dentro delas temos acesso a variável __dirname (diretório atual), então, para ser compatível com Linux, Windows, MacOS, etc... o resolve irá resolver o jeito de colocar as barras, então colocamos apenas os nomes das pastas que ele vai passar:
-
-    Ex: __dirname(pasta atual) -> 'public' -> 'assets' -> 'js'
-    */  
+    devtool: 'source-map'
 };
