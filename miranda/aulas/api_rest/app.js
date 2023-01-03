63044b8c356bc34e0cf80b515c0d1c6d19c40340
= <92165034+pvsanches@users.noreply.github.com>
2023-01-03 19:18:35 -0300

    adicionando api_rest sobre eslint

---
+++ miranda/aulas/api_rest/app.js
@@ -0,0 +1,7 @@
+const teste = 22;
+console.log(teste);
+
+// Assim, podemos desativar o erro via comentário
+// console.log(teste); // eslint-disable-line
+// eslint-disable-next-line
+// console.log(teste);
